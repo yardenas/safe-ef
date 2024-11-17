29a35f1bb2f35778e54888571105075c199fa0a8
Yarden <yarden.as@inf.ethz.ch>
2024-11-17 14:19:32 +0100

    Cartpole

--- ef14/benchmark_suites/__init__.py
+++ ef14/benchmark_suites/__init__.py
@@ -5,20 +5,15 @@ from brax import envs
 
 from ef14.benchmark_suites import brax
 from ef14.benchmark_suites.brax.cartpole import cartpole
-from ef14.benchmark_suites.rccar import rccar
 from ef14.benchmark_suites.utils import get_domain_name, get_task_config
-from ef14.benchmark_suites.wrappers import (
-    ActionObservationDelayWrapper,
-    FrameActionStack,
-)
 
 
 def make(cfg):
     domain_name = get_domain_name(cfg)
     if domain_name == "brax":
         return make_brax_envs(cfg)
-    elif domain_name == "rccar":
-        return make_rccar_envs(cfg)
+    else:
+        raise ValueError(f"Unknown domain name {domain_name}")
 
 
 def prepare_randomization_fn(key, num_envs, cfg, task_name):
@@ -28,67 +23,6 @@ def prepare_randomization_fn(key, num_envs, cfg, task_name):
     )
     vf_randomization_fn = lambda sys: v_randomization_fn(sys)[:-1]  # type: ignore
     return vf_randomization_fn
-
-
-def make_rccar_envs(cfg):
-    task_cfg = dict(get_task_config(cfg))
-    task_cfg.pop("domain_name")
-    task_cfg.pop("task_name")
-    train_car_params = task_cfg.pop("train_car_params")
-    eval_car_params = task_cfg.pop("eval_car_params")
-    train_key, eval_key = jax.random.split(jax.random.PRNGKey(cfg.training.seed))
-    action_delay, obs_delay = (
-        task_cfg.pop("action_delay"),
-        task_cfg.pop("observation_delay"),
-    )
-    sliding_window = task_cfg.pop("sliding_window")
-    train_env = rccar.RCCar(train_car_params["nominal"], **task_cfg)
-    if action_delay > 0 or obs_delay > 0:
-        train_env = ActionObservationDelayWrapper(
-            train_env, action_delay=action_delay, obs_delay=obs_delay
-        )
-    if sliding_window > 0:
-        train_env = FrameActionStack(train_env, num_stack=sliding_window)
-    train_randomization_fn = (
-        prepare_randomization_fn(
-            train_key,
-            cfg.training.num_envs,
-            train_car_params["bounds"],
-            cfg.environment.task_name,
-        )
-        if cfg.training.train_domain_randomization
-        else None
-    )
-    train_env = envs.training.wrap(
-        train_env,
-        episode_length=cfg.training.episode_length,
-        action_repeat=cfg.training.action_repeat,
-        randomization_fn=train_randomization_fn,
-    )
-    eval_env = rccar.RCCar(eval_car_params["nominal"], **task_cfg)
-    if action_delay > 0 or obs_delay > 0:
-        eval_env = ActionObservationDelayWrapper(
-            eval_env, action_delay=action_delay, obs_delay=obs_delay
-        )
-    if sliding_window > 0:
-        eval_env = FrameActionStack(eval_env, num_stack=sliding_window)
-    eval_randomization_fn = (
-        prepare_randomization_fn(
-            eval_key,
-            cfg.training.num_eval_envs,
-            eval_car_params["bounds"],
-            cfg.environment.task_name,
-        )
-        if cfg.training.eval_domain_randomization
-        else None
-    )
-    eval_env = envs.training.wrap(
-        eval_env,
-        episode_length=cfg.training.episode_length,
-        action_repeat=cfg.training.action_repeat,
-        randomization_fn=eval_randomization_fn,
-    )
-    return train_env, eval_env
 
 
 def make_brax_envs(cfg):
@@ -129,11 +63,8 @@ randomization_fns = {
     "cartpole_swingup": cartpole.domain_randomization,
     "cartpole_swingup_sparse": cartpole.domain_randomization,
     "cartpole_balance": cartpole.domain_randomization,
-    "inverted_pendulum": cartpole.domain_randomization,
-    "rccar": rccar.domain_randomization,
 }
 
 render_fns = {
     "cartpole_swingup": brax.render,
-    "rccar": rccar.render,
 }
