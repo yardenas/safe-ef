c66bb4f041a9889a67c81785fc419e91bde196d5
Yarden <yarden.as@inf.ethz.ch>
2024-11-22 09:08:42 +0100

    Add cost scaling

--- ef14/algorithms/sac/train.py
+++ ef14/algorithms/sac/train.py
@@ -144,6 +144,7 @@ def train(
     normalize_observations: bool = False,
     max_devices_per_host: Optional[int] = None,
     reward_scaling: float = 1.0,
+    cost_scaling: float = 1.0,
     tau: float = 0.005,
     min_replay_size: int = 0,
     max_replay_size: Optional[int] = None,
@@ -288,6 +289,7 @@ def train(
     alpha_loss, critic_loss, actor_loss = sac_losses.make_losses(
         sac_network=sac_network,
         reward_scaling=reward_scaling,
+        cost_scaling=cost_scaling,
         discounting=discounting,
         safety_discounting=safety_discounting,
         action_size=action_size,
